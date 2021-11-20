3137c705e5781a7f5c866bb24e5d0bbc9622f3f5
sgpx <spgithubacc@gmail.com>
2021-11-20 08:56:05 +0530

    initial commit

---
+++ musicdb.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import easy_dynamodb.easy_dynamodb as edb
+
+from bs4 import BeautifulSoup
+import requests
+import pprint
+from sys import argv
+
+def get_title(link):
+    resp = requests.get(link).text
+    soup = BeautifulSoup(resp, features="html.parser")
+    title = soup.title.text
+    return title
+
+
+def submit_link(link):
+    title = get_title(link)
+    print(edb.dynamodb_client.put_item(
+        TableName="music_db",
+        Item={"link": {"S": link}, "title": {"S": title if title else ""}},
+    ))
+
+def get_all_links():
+    pprint.pprint(edb.get_all_items("music_db"))
+
+if __name__ == "__main__":
+    arg = argv[-1]
+    if arg in ["-l","--list","list"]:
+        get_all_links()
+    else:
+        submit_link(arg)
