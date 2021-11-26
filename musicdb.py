9f4b23582cb9b38d574d725c7702d25c8774d2d7
SP <spgithubacc@gmail.com>
2021-11-26 13:22:02 +0530

    2021-11-26-13-22

--- musicdb.py
+++ musicdb.py
@@ -3,12 +3,16 @@ import easy_dynamodb.easy_dynamodb as edb
 
 from bs4 import BeautifulSoup
 import requests
-import pprint
 from sys import argv
-import datetime
 
-def get_timestamp():
-    return datetime.datetime.now().isoformat()
+def print_item(x):
+    print("======")
+    for field in x:
+        print(f"{field} : '{x.get(field,{}).get('S')}'")
+
+def pprint(x):
+    for i in x:
+        print_item(i)
 
 def get_title(link):
     resp = requests.get(link).text
@@ -19,13 +23,14 @@ def get_title(link):
 
 def submit_link(link):
     title = get_title(link)
+    print(title)
     print(edb.dynamodb_client.put_item(
         TableName="music_db",
-        Item={"link": {"S": link}, "title": {"S": title if title else ""}, "timestamp":{"S":get_timestamp()}},
+        Item={"link": {"S": link}, "title": {"S": title if title else ""}},
     ))
 
 def get_all_links():
-    pprint.pprint(edb.get_all_items("music_db"))
+    pprint(edb.get_all_items("music_db"))
 
 if __name__ == "__main__":
     arg = argv[-1]
