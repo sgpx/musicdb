b55b541e7a4bc4c4e2267a1355470bdf3e55ee12
SP <spgithubacc@gmail.com>
2021-12-03 17:34:34 +0530

    2021-12-03-17-34

--- run.sh
+++ run.sh
@@ -2,8 +2,10 @@
 a=$(find . -iname venv -depth 1)
 if [ "$a" = "./venv" ]; then
 	echo found venv
-	source venv/bin/activate
-	python3 musicdb.py $@
 else
-	echo not found
+	echo venv not found, creating...
+	bash install_deps.sh
 fi
+
+source venv/bin/activate
+python3 musicdb.py $@
